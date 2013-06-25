1ceb9cd93fada6e1205e1e154e0e6e384cf824fa
tboeckmann <trb@bespecific.co.uk>
2013-06-25 12:47:06 +0100

    task modal

---
+++ public/javascripts/task-scripts.js
@@ -0,0 +1,42 @@
+// Sortable script
+  $(function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  });
+
+// Datepicker script
+  $(function() {
+    $( ".datepicker" ).datepicker();
+  });
+
+
+// Create task modal
+$(function() {
+  var description = $( "#description" ),
+    due = $( "#due" );
+
+  $( "#dialog-form" ).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 800,
+    modal: true,
+    buttons: {
+      "Create Task": function() {
+        $( "#sortable").append( "<li class=\"ui-state-default\"><span class=\"ui-icon ui-icon-arrowthick-2-n-s\"></span>" + description.val() + " " + due.val() + "<button type=\"button\">Edit</button></li>");
+        $("#description").val("");
+        // $("#due").val()
+        $( this ).dialog( "close" );
+      },
+      Cancel: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  });
+
+// Create task button for modal
+  $( "#create-task" )
+    .button()
+    .click(function() {
+      $( "#dialog-form" ).dialog( "open" );
+  });
+});
