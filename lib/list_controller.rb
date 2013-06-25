f9d3d639f932138469a94df69407a7022ec12f16
James Lieu <j.lieu888@gmail.com>
2013-06-25 10:31:44 +0100

    session data structure agreed and defined in comments

--- lib/list_controller.rb
+++ lib/list_controller.rb
@@ -5,10 +5,44 @@ class ListController < Sinatra::Base
 
   configure do
     set :root, Proc.new { File.join(File.dirname(__FILE__), "../") }
+
+    use Rack::Session::Cookie, :key => 'rack.session',
+                               :path => '/',
+                               :expire_after => 2592000,
+                               :secret => 'Charlotte Neil Tim James B Team'
   end
+
+# session = {
+#   :task_count => ? ,
+#   :tasks      => {
+#                   {:id          => ?,
+#                   :description  => ?,
+#                   :due          => ?,
+#                   :complete     =>?}
+#                   ,
+#                   {:id          => ?,
+#                   :description  => ?,
+#                   :due          => ?,
+#                   :complete     =>?}
+#                   }
+#           }
+# To access the task count in the session:
+#   session[:task_count]
+# To access all tasks in the session:
+#   session[:tasks]
+# To access a particular task in the session:
+#   use the find_task(:id)
+
+  # def find_task(id)
+  # end
 
   get '/' do
     erb :main
+  end
+
+  post '/create_task' do
+    # fill this in later
+
   end
 
   post '/delete_task' do
@@ -19,7 +53,5 @@ class ListController < Sinatra::Base
     # fill this in later
   end
 
-  post '/create_task' do
-    # fill this in later
-  end
+
 end
