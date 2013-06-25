d47bbe4547797a9b8db3f5dbcbff5fcea8b763eb
James Lieu <j.lieu888@gmail.com>
2013-06-25 09:50:00 +0100

    Added the actions and the tests for them

--- lib/list_controller.rb
+++ lib/list_controller.rb
@@ -8,8 +8,18 @@ configure do
 end
 
   get '/' do
-    
     erb :main
   end
 
+  post '/delete_task' do
+    # fill this in later
+  end
+
+  post '/update_task' do
+    # fill this in later
+  end
+
+  post '/create_task' do
+    # fill this in later
+  end
 end
--- test/list_controller_test.rb
+++ test/list_controller_test.rb
@@ -6,15 +6,28 @@ require_relative '../lib/list_controller'
 class ListControllerTest < Minitest::Test
 include Rack::Test::Methods
 
-def app
-  ListController.new
-end
+  def app
+    ListController.new
+  end
 
-def test_has_a_root_controller_which_returns_the_main_page
-  get '/'
-  assert last_response.ok?
-  assert_includes last_response.body, 'Task List'
-end
+  def test_has_a_root_controller_which_returns_the_main_page
+    get '/'
+    assert last_response.ok?
+    assert_includes last_response.body, 'Task List'
+  end
 
+  def test_can_delete_a_task
+    post '/delete_task'
+    assert last_response.ok?
+  end
 
+  def test_can_update_task
+    post '/update_task'
+    assert last_response.ok?
+  end
+
+  def test_can_create_tasks
+    post '/create_task'
+    assert last_response.ok?
+  end
 end
