99920f261073abb889ef775b28efffb096a74887
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-06-28 10:16:35 +0100

    rectified errors in capybara tests

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -19,12 +19,22 @@ describe 'home page', :type => :feature, :js => true do
     click_button "New Task"
     fill_in "Task Number", :with => "1"
     fill_in "Description", :with => "Buy honey"
-    page.check('completed')
+    fill_in "Due", :with => "23-05-2013"
+    page.check('create_completed')
     click_button "Create Task"
     within("#sortable") do     
-      page.should have_content('Buy honey')
+      page.should have_content("Buy honey")
     end
   end
+    # click_button "New Task"
+    # fill_in "Task Number", :with => task_no
+    # fill_in "Description", :with => description
+    # fill_in "Due", :with => "23-05-2013"
+    # page.check('create_completed') if completed
+    # click_button "Create Task"
+    # within("#sortable") do     
+    #   page.should have_content(description)
+    # end
 
   it 'displays tasks that were created in a previous visit' do
     visit '/'
@@ -37,6 +47,7 @@ describe 'home page', :type => :feature, :js => true do
       page.should have_content("get job")
       page.should have_content("write a book")
     end
+    # page.body.index(new_comment.text).should < page.body.index(old_comment.text)
   end
 
   describe "a task on the page" do
@@ -44,12 +55,15 @@ describe 'home page', :type => :feature, :js => true do
     describe "update button" do
 
       it 'allows to update a task' do
-        visit '/'    
+        visit '/'
+        create_new_task(1, "buy milk", "15-05-2013", true)
         click_button "Edit"
         fill_in "Description", :with => "Buy cheese"
+        fill_in "Due", :with => "23-05-2013"
+        page.check('create_completed')
         click_button "Update Task"
         within("#sortable") do     
-          page.should have_content('Buy cheese')
+          page.should have_content("Buy cheese")
         end
       end
     end
